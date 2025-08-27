e085024761fb751ad4ab3b95d9585c1a18fd3619
jasen <jasenkoff.mack@yandex.ru>
2025-08-27 17:16:21 +0300

    nothing2

---
+++ src/DZ_4.java
@@ -0,0 +1,18 @@
+public class DZ_4 {
+    public static void main(String[] args) {
+        // Задача 1
+        int[] array = {1, 3, 4, 5, 7, 9};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i]);
+        }
+
+        // Задча 2
+        int[] array2 = {5, 7, 89, 43, 1, 94, 13};
+        int minimal = Math.max(array2);
+
+
+    }
+}
