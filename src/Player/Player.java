290e34d535e1b19de230006945d440044f283b6c
jasen <jasenkoff.mack@yandex.ru>
2025-09-04 15:48:00 +0300

    draft have done ++

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -8,16 +8,7 @@ public class Player {
         System.out.println("Игрок " + playerName + " атакует используя " + weapon.weaponName +
                 ", нанося " + weapon.damage + " урона.");
     }
-
-    public static class Weapon {
-    String weaponName;
-    int damage;
-
-        public Weapon(int damage, String weaponName) {
-            this.damage = damage;
-            this.weaponName = weaponName;
-        }
-    }
 }
+
 
 
---
+++ src/Player/Weapon.java
@@ -0,0 +1,11 @@
+package Player;
+
+public class Weapon {
+    String weaponName;
+    int damage;
+
+    public Weapon(String weaponName, int damage) {
+        this.weaponName = weaponName;
+        this.damage = damage;
+    }
+}
