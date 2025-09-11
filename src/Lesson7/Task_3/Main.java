18d0617d3fe9635df1dfbe8c96d476f1f14d1597
jasen <jasenkoff.mack@yandex.ru>
2025-09-11 20:55:55 +0300

    cool

--- src/Lesson7/Task_3/Main.java
+++ src/Lesson7/Task_3/Main.java
@@ -2,7 +2,8 @@ package Lesson7.Task_3;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person();
+        Person person = new Person("Maxim", 26, "man");
+        System.out.println(person.getAge()+ " " + person.getName() + " " + person.getGender());
 
     }
 }
--- src/Lesson7/Task_3/Person.java
+++ src/Lesson7/Task_3/Person.java
@@ -4,4 +4,22 @@ public class Person {
     public String name;
     int age;
     private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
