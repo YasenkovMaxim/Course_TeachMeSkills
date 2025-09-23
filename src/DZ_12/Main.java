d0ccbab3b29352900ef438b5d423f30dab1193e4
jasen <jasenkoff.mack@yandex.ru>
2025-09-23 19:44:24 +0300

    регулярные выражения

--- src/DZ_12/Main.java
+++ src/DZ_12/Main.java
@@ -11,12 +11,12 @@ public class Main {
          аббревиатуры. Аббревиатурой будем считать слово от 2 до 6 символов, состоящее
          только из прописных букв, без чисел. */
         Scanner scanner = new Scanner(System.in);
-       /* String text = scanner.nextLine();
+        String text = scanner.nextLine();
         Pattern pattern = Pattern.compile("[A-ZА-Я]{2,6}");
         Matcher m = pattern.matcher(text);
         while (m.find()) {
             System.out.println(m.group());
-        }*/
+        }
 
         /** Задача *:
          Программа на вход получает произвольный текст. В этом тексте может быть номер
@@ -29,11 +29,23 @@ public class Main {
          email: teachmeskills@gmail.com
          document number: 1423-1512-51
          и т.д */
-        String doc = scanner.nextLine();
+        String doc = "Уважаемый сотрудник, Мои контактные данные: Email: jane-doe@mail-example.com" +
+                "Документ: 2018-9876-54, прошу принять к сведению. Телефон: +(34)98765432 " +
+                "Благодарю за помощь.";
         Pattern pattern2 = Pattern.compile("\\d{4}-\\d{4}-\\d{2}");
         Matcher m2 = pattern2.matcher(doc);
-        while (m2.find()){
+        while (m2.find()) {
             System.out.println("document number: " + m2.group());
+        }
+        Pattern pattern3 = Pattern.compile("[\\w-.]+@[\\w-.]+\\.[\\w-.]+");
+        Matcher m3 = pattern3.matcher(doc);
+        while (m3.find()) {
+            System.out.println("email: " + m3.group());
+        }
+        Pattern pattern4 = Pattern.compile("\\+\\(\\d{2}\\)\\d{7}");
+        Matcher m4 = pattern4.matcher(doc);
+        while (m4.find()) {
+            System.out.println("phone number: " + m4.group());
         }
     }
 }
